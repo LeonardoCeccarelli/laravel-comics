c8226c362797755db3e3a056d829a7e1a8a22da8
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-12-21 14:29:48 +0100

    change position in array from web to icon main bottom

--- resources/views/partials/icon_main_bottom.blade.php
+++ resources/views/partials/icon_main_bottom.blade.php
@@ -1,3 +1,30 @@
+@php
+
+$listIconMainBottom = [
+  [
+      "text" => "Digital comics",
+      "src" => "buy-comics-digital-comics.png",
+  ],
+  [
+      "text" => "Dc merchandise",
+      "src" => "buy-comics-merchandise.png",
+  ],
+  [
+      "text" => "Subscription",
+      "src" => "buy-comics-subscriptions.png",
+  ],
+  [
+      "text" => "Comic shop locator",
+      "src" => "buy-comics-shop-locator.png",
+  ],
+  [
+      "text" => "Dc power visa",
+      "src" => "buy-dc-power-visa.svg",
+  ]
+];
+
+@endphp
+
 <div class="info_footer">
     <ul>
       @foreach ($listIconMainBottom as $icon)
