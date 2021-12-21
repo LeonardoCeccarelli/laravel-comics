f35d9783f1a0362bd2ec0ac44d6a25a8607abb21
LeonardoCeccarelli <leo.ceccarelli@tiscali.it>
2021-12-21 10:50:05 +0100

    formatt data

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -1,3 +1,50 @@
+@php
+
+$listNavLink = [
+  [
+      "text" => "Characters",
+      "link" => "#Characters",
+  ],
+  [
+      "text" => "Comics",
+      "link" => "#Comics",
+  ],
+  [
+      "text" => "Movies",
+      "link" => "#Movies",
+  ],
+  [
+      "text" => "Tv",
+      "link" => "#Tv",
+  ],
+  [
+      "text" => "Games",
+      "link" => "#Games",
+  ],
+  [
+      "text" => "Collectibles",
+      "link" => "#Collectibles",
+  ],
+  [
+      "text" => "Videos",
+      "link" => "#Videos",
+  ],
+  [
+      "text" => "Fans",
+      "link" => "#Fans",
+  ],
+  [
+      "text" => "News",
+      "link" => "#News",
+  ],
+  [
+      "text" => "Shop",
+      "link" => "#Shop",
+  ],
+];
+
+@endphp
+
 <nav class="main_navbar">
     <div class="logo_navbar">
       <img src="{{ asset('img/dc-logo.png') }}" alt="DC logo"/>
